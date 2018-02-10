8b2c9ec1302c1e49210a9c95fc7a578d02bad7ca
Reza.Abdolahi <rz.ab.67@gmail.com>
2018-02-11 01:42:29 +0330

    Initial Commit

---
+++ HighResMasking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HighResMasking
+//
+//  Created by Reza on 2/11/18.
+//  Copyright © 2018 manamaJeff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HighResMasking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HighResMasking
+//
+//  Created by Reza on 2/11/18.
+//  Copyright © 2018 manamaJeff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
